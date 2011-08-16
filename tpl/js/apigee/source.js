700ec37371b0e15675dfc27ae827aeb3d7100e60
Egor Pomortsev <egor@pomortsev.com>
2011-08-16 15:21:07 -0700

    Encapsulate SourceApplication using John Resig's Simple JavaScript Inheritance class framework

--- tpl/js/apigee/source.js
+++ tpl/js/apigee/source.js
@@ -1,131 +1,135 @@
-function SourceApplication(appName) {
-    var theApp = this;
-    this.userObject = {};
-    this.user_authenticated = false;
+var SourceApplication = Class.extend({
+    init: function(appName) {
+        this.api = $.apigee_api('https://' + appName + '-api.apigee.com/v1');
+        this.appName = appName;
 
-    this.getAuth = function() {
-        if (theApp.api && theApp.api.doesLocalStorage) theApp.logIn();
-    };
+        this.userObject = {};
+        this.user_authenticated = false;
+    },
 
-    this.processSmartKey = function(data) {
+    getAuth: function() {
+        if (this.api && this.api.doesLocalStorage) this.logIn();
+    },
+
+    processSmartKey: function(data) {
         if (data && data.payload) {
             data = $.parseJSON(data.payload);
-            theApp.setAuth(data);
-            theApp.logIn();
+            this.setAuth(data);
+            this.logIn();
         }
-    };
+    },
 
-    this.processKeyAndSecret = function(data) {
+    processKeyAndSecret: function(data) {
         if (data && data.payload) {
             data = $.parseJSON(data.payload);
             if (data.hasOwnProperty('oauthToken') && data.hasOwnProperty('oauthTokenSecret')) {
-                theApp.setAuth(data);
-                theApp.logIn();
+                this.setAuth(data);
+                this.logIn();
             } else {
-                theApp.showResponseMessage('Please re-try your login credentials or create a new account');
+                this.showResponseMessage('Please re-try your login credentials or create a new account');
             }
         }
-    };
+    },
 
-    this.setAuth = function(authObject) {
+    setAuth: function(authObject) {
         for (var key in authObject) {
             if (authObject.hasOwnProperty(key)) {
-                theApp.userObject[key] = authObject[key];
-                if (theApp.api && theApp.api.doesLocalStorage) localStorage[key] = authObject[key];
-                if (key == 'smartKey') theApp.api.setSmartKey(authObject.smartKey);
+                this.userObject[key] = authObject[key];
+                if (this.api && this.api.doesLocalStorage) localStorage[key] = authObject[key];
+                if (key == 'smartKey') this.api.setSmartKey(authObject.smartKey);
             }
         }
-    };
+    },
 
-    this.logIn = function(username, password) {
-        if (theApp.api) {
+    logIn: function(username, password) {
+        if (this.api) {
             var goodUser = true;
             var userInfo = [];
 
             if (username && password) {
                 userInfo = [username, password];
-            } else if (theApp.api.doesLocalStorage && localStorage.authorization) {
+            } else if (this.api.doesLocalStorage && localStorage.authorization) {
                 userInfo = $.base64Decode(localStorage.authorization).split(':');
             }
 
             if (userInfo.length == 2) {
-                theApp.setAuth({
+                this.setAuth({
                     'displayname': userInfo[0],
                     'username': userInfo[0],
                     'password': userInfo[1],
                     'authorization': $.base64Encode(userInfo[0] + ':' + userInfo[1])
                 });
 
-                theApp.api.init(userInfo[0], userInfo[1]);
+                this.api.init(userInfo[0], userInfo[1]);
             } else {
-                theApp.showResponseMessage('Please include a username and a password.');
+                this.showResponseMessage('Please include a username and a password.');
                 goodUser = false;
             }
 
-            if ((theApp.api.doesLocalStorage && (localStorage.smartkey || localStorage.smartKey)) || (theApp.userObject.smartKey || theApp.userObject.smartkey)) {
+            if ((this.api.doesLocalStorage && (localStorage.smartkey || localStorage.smartKey)) || (this.userObject.smartKey || this.userObject.smartkey)) {
                 var theSmartKey;
-                if (theApp.api.doesLocalStorage && (localStorage.smartkey || localStorage.smartKey)) {
+                if (this.api.doesLocalStorage && (localStorage.smartkey || localStorage.smartKey)) {
                     theSmartKey = (localStorage.smartkey) ? localStorage.smartkey : localStorage.smartKey;
                 } else {
-                    theSmartKey = (theApp.userObject.smartkey) ? theApp.userObject.smartkey : theApp.userObject.smartKey;
+                    theSmartKey = (this.userObject.smartkey) ? this.userObject.smartkey : this.userObject.smartKey;
                 }
-                theApp.setAuth({'smartKey':theSmartKey,'smartkey':theSmartKey});
-            } else if (theApp.api.authorization) {
-                theApp.api.request('get', '/smartkeys/me.json?uid=' + theApp.api.authorization, {}, {callback: theApp.processSmartKey});
+                this.setAuth({'smartKey':theSmartKey,'smartkey':theSmartKey});
+            } else if (this.api.authorization) {
+                this.api.request('get', '/smartkeys/me.json?uid=' + this.api.authorization, {}, {callback: this.processSmartKey});
                 goodUser = false;
             } else {
-                theApp.showResponseMessage('Please re-try your login credentials or create a new account');
+                this.showResponseMessage('Please re-try your login credentials or create a new account');
                 goodUser = false;
             }
 
-            if ((theApp.api.doesLocalStorage && (localStorage.oauthToken && localStorage.oauthTokenSecret)) || (theApp.userObject.oauthToken && theApp.userObject.oauthTokenSecret)) {
+            if ((this.api.doesLocalStorage && (localStorage.oauthToken && localStorage.oauthTokenSecret)) || (this.userObject.oauthToken && this.userObject.oauthTokenSecret)) {
                 var theToken, theSecret;
-                if (theApp.api.doesLocalStorage && (localStorage.oauthToken && localStorage.oauthTokenSecret)) {
+                if (this.api.doesLocalStorage && (localStorage.oauthToken && localStorage.oauthTokenSecret)) {
                     theToken = localStorage.oauthToken;
                     theSecret = localStorage.oauthTokenSecret;
                 } else {
-                    theToken = theApp.userObject.oauthToken;
-                    theSecret = theApp.userObject.oauthTokenSecret;
+                    theToken = this.userObject.oauthToken;
+                    theSecret = this.userObject.oauthTokenSecret;
                 }
-                theApp.setAuth({'oauthToken':theToken,'oauthTokenSecret':theSecret});
-            } else if (theApp.userObject.smartKey) {
-                theApp.api.request('get', '/smartkeys/' + theApp.userObject.smartKey + '/providers/twitter.json', {}, {callback:theApp.processKeyAndSecret});
+                this.setAuth({'oauthToken':theToken,'oauthTokenSecret':theSecret});
+            } else if (this.userObject.smartKey) {
+                this.api.request('get', '/smartkeys/' + this.userObject.smartKey + '/providers/twitter.json', {}, {callback:this.processKeyAndSecret});
                 goodUser = false;
             } else {
                 goodUser = false;
             }
 
-            theApp.user_authenticated = goodUser;
+            this.user_authenticated = goodUser;
         }
-    };
+    },
 
-    this.logOut = function() {
+    logOut: function() {
         var userCredentials = ['authorization'];
 
-        for (var key in theApp.userObject) {
-            if (theApp.userObject.hasOwnProperty(key)) {
+        for (var key in this.userObject) {
+            if (this.userObject.hasOwnProperty(key)) {
                 userCredentials.push(key);
             }
         }
 
         for (var i = 0; i < userCredentials.length; i++) {
             var thisCredential = userCredentials[i];
-            if (theApp.api) {
-                if (theApp.api.doesLocalStorage) localStorage.removeItem(thisCredential);
-                if (theApp.api[thisCredential]) delete theApp.api[thisCredential];
-                if (theApp.userObject[thisCredential]) delete theApp.userObject[thisCredential];
+            if (this.api) {
+                if (this.api.doesLocalStorage) localStorage.removeItem(thisCredential);
+                if (this.api[thisCredential]) delete this.api[thisCredential];
+                if (this.userObject[thisCredential]) delete this.userObject[thisCredential];
             }
         }
 
-        theApp.user_authenticated = false;
-    };
+        this.user_authenticated = false;
+    },
 
-    this.createAccount = function(username, password) {
-        if (theApp.api && username && password) {
-            if (theApp.api.doesLocalStorage)
+    createAccount: function(username, password) {
+        if (this.api && username && password) {
+            if (this.api.doesLocalStorage)
                 localStorage.authorization = $.base64Encode(username + ':' + password);
 
-            theApp.api.request(
+            this.api.request(
                 'post',
                 '/users.json',
                 {
@@ -133,41 +137,41 @@ function SourceApplication(appName) {
                     'fullName': username,
                     'password': password
                 },
-                { callback: theApp.newAccount }
+                { callback: this.newAccount }
             );
         } else {
-            theApp.showResponseMessage('Please include a username and a password.');
+            this.showResponseMessage('Please include a username and a password.');
         }
-    };
+    },
 
-    this.newAccount = function(data) {
-        var responsePayload = ((typeof theApp.api.returnObject.payload) === "string") ? $.parseJSON(theApp.api.returnObject.payload) : theApp.api.returnObject.payload;
+    newAccount: function(data) {
+        var responsePayload = ((typeof this.api.returnObject.payload) === "string") ? $.parseJSON(this.api.returnObject.payload) : this.api.returnObject.payload;
         if (responsePayload.hasOwnProperty('smartKey')) {
-            if (theApp.api.doesLocalStorage) localStorage.smartkey = responsePayload.smartKey;
+            if (this.api.doesLocalStorage) localStorage.smartkey = responsePayload.smartKey;
             var currentHref = document.location.href;
-            document.location.href = theApp.api.defaults.endpoint + '/providers/twitter/authorize?smartkey=' + responsePayload.smartKey + '&app_callback=' + currentHref;
+            document.location.href = this.api.defaults.endpoint + '/providers/twitter/authorize?smartkey=' + responsePayload.smartKey + '&app_callback=' + currentHref;
         }
-    };
+    },
 
-    this.sendRequest = function(verb, request, headers, settings) {
+    sendRequest: function(verb, request, headers, settings) {
         verb = verb || "get";
         headers = headers || {};
         settings = settings || {};
 
-        if (theApp.api && request) {
-            if (theApp.api.smartkey && (verb == 'get')) {
+        if (this.api && request) {
+            if (this.api.smartkey && (verb == 'get')) {
                 var smartKeyDivider = (request.indexOf('?') != -1) ? '&' : '?';
-                request += (smartKeyDivider + 'smartkey=' + theApp.api.smartkey);
+                request += (smartKeyDivider + 'smartkey=' + this.api.smartkey);
             }
-            theApp.api.request(verb, request, headers, settings);
+            this.api.request(verb, request, headers, settings);
         }
-    };
+    },
 
     /** @private */
-    this.showResponseMessage = function(theMessage) {
+    showResponseMessage: function(theMessage) {
         theMessage = theMessage.replace(/<\/?[^>]+>/gi, '');
 
-        if (theApp.api && theApp.api.returnObject && theApp.api.returnObject.xhr && theApp.api.returnObject.xhr.responseText) {
+        if (this.api && this.api.returnObject && this.api.returnObject.xhr && this.api.returnObject.xhr.responseText) {
             function parseAndReturn(theText) {
                 var theJson = '';
                 try {
@@ -178,17 +182,10 @@ function SourceApplication(appName) {
                 return theJson;
             }
 
-            var responseMessage = parseAndReturn(theApp.api.returnObject.xhr.responseText);
+            var responseMessage = parseAndReturn(this.api.returnObject.xhr.responseText);
             if (responseMessage.hasOwnProperty('message')) theMessage = responseMessage.message;
         }
 
         console.log(theMessage);
-    };
-
-    this.init = function(appName) {
-        theApp.api = $.apigee_api('https://' + appName + '-api.apigee.com/v1');
-        theApp.appName = appName;
-    };
-
-    if (appName) theApp.init(appName);
-}
+    }
+});
